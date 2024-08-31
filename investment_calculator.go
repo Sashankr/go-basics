d89e42ec852fba61116d33c36fdaf330df0382cb
Sashankr <sashankr97@gmail.com>
2024-08-31 23:14:33 +0530

    const variable usage

--- investment_calculator.go
+++ investment_calculator.go
@@ -14,6 +14,7 @@ func main(){
 	futureValue := investmentAmount * math.Pow((1 + (expectedReturnRate /  100)),years); 
 	futureRealValue := futureValue / math.Pow(1 + (inflationRate / 100),years)
 
+	fmt.Println(futureValue)
 
 	fmt.Println(futureRealValue)
 } 
