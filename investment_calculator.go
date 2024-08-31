3e623cf14fdf8c33df42b6f3a373722a07221b26
Sashankr <sashankr97@gmail.com>
2024-08-31 22:57:06 +0530

    alternative declaration syntax

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main(){
-	var investmentAmount float64= 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	var investmentAmount float64 = 1000
+	expectedReturnRate := 5.5
+	years  := 10.0
 
 	var futureValue = investmentAmount * math.Pow((1 + (expectedReturnRate /  100)),years); 
 	fmt.Println(math.Round(futureValue))
