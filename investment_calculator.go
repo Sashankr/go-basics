3c3b838e879ddd336bbb8ec03e4ca47d2f63e941
Sashankr <sashankr97@gmail.com>
2024-09-01 20:17:26 +0530

    wrote profit calculator function

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,7 +5,38 @@ import (
 	"math"
 )
 
-func main(){
+func profit_calculator(){
+	// inputs: revenue, expenses, tax_rate
+	// outputs : EBT, Earning after tax(profit)
+	// outputs : ratio(EBT/profit)
+	// Final outputs : EBT, profit and ratio
+
+	var revenue float64
+	var expenses float64
+	var taxRate float64
+
+	fmt.Print("Enter your revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter your expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Enter Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+
+	earningsBeforeTax := revenue - expenses;
+	fmt.Println("Earnings Before Tax: ",earningsBeforeTax);
+
+	earningsAfterTax := earningsBeforeTax - ((taxRate/100)*earningsBeforeTax)
+	fmt.Println("Earnings After Tax: ", earningsAfterTax)
+
+	ratio := earningsBeforeTax / earningsAfterTax
+	fmt.Println("Ratio: ", ratio);
+
+}
+
+func investment_calculator(){
 	const inflationRate = 2.5
 	var investmentAmount float64 
 	var expectedReturnRate float64
@@ -26,4 +57,9 @@ func main(){
 	fmt.Println(futureValue)
 
 	fmt.Println(futureRealValue)
+}
+
+func main(){
+	// investment_calculator()
+	profit_calculator()
 } 
