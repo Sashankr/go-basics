8e4e1d7b31a714008d7ec7bc3e5878484b1f0dcb
Sashankr <sashankr97@gmail.com>
2024-09-01 19:19:00 +0530

    fmt Scan method

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,9 +7,12 @@ import (
 
 func main(){
 	const inflationRate = 2.5
-	var investmentAmount float64 = 1000
+	var investmentAmount float64
 	expectedReturnRate := 5.5
 	years  := 10.0
+	
+	fmt.Println("Enter the investment amount:")
+	fmt.Scan(&investmentAmount);
 
 	futureValue := investmentAmount * math.Pow((1 + (expectedReturnRate /  100)),years); 
 	futureRealValue := futureValue / math.Pow(1 + (inflationRate / 100),years)
